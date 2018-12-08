2118ab24902d8dbe0bfe7b89ecea7b8b4c54c28c
george <george-mamardashvili@yandex.ru>
2018-12-08 18:33:23 +0300

    added topic page

---
+++ app/src/main/java/com/example/george/drilldown_android/TopicItemActivity.java
@@ -0,0 +1,96 @@
+package com.example.george.drilldown_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.*;
+import android.webkit.ConsoleMessage;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class TopicItemActivity extends AppCompatActivity {
+
+    private String baseUrl = "http://10.0.2.2:8080";
+    private String topicId = "5b52d055107696026b43b44d";
+
+    ImageView topicImageView;
+    TextView topicTitleView;
+    TextView topicDescView;
+    Button startLessonButton;
+
+    RequestQueue mQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topic_item);
+
+        topicImageView = (ImageView) findViewById(R.id.topicImageView);
+        topicTitleView = (TextView) findViewById(R.id.topicTitleView);
+        topicDescView = (TextView) findViewById(R.id.topicDescView);
+        startLessonButton = (Button) findViewById(R.id.startLessonButton);
+
+        startLessonButton.setOnClickListener(startLesson);
+
+        mQueue = Volley.newRequestQueue(this);
+        getTopic();
+    }
+
+    OnClickListener startLesson = new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+        }
+    };
+
+    private void getTopic(){
+        String url = baseUrl + "/topics/" + topicId;
+        Log.i("ok","start");
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Log.i("ok","here");
+
+                    Log.i("ok",response.toString());
+
+                    JSONObject topic = response.getJSONObject("topic");
+                    String title = topic.getString("title");
+                    String image = topic.getString("image");
+                    String desc = topic.getString("about");
+
+                    Picasso.get().load(baseUrl + "/" + image).into(topicImageView);
+                    topicTitleView.setText(title);
+                    topicDescView.setText(desc);
+                } catch (JSONException e) {
+
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("ok","not here");
+
+                //error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
+    }
+
+
+}
